ce97daa50bab89499e2cac01c16d500f1c85af52
Monsterray <inkrpen@gmail.com>
2017-04-02 17:18:25 -0700

    Tons of work
    Did a whole ton of work on the project, currently working on learing how
    to use the touch screen and getting variables added.

--- TouchScreenMenu/TouchScreenMenu.ino
+++ TouchScreenMenu/TouchScreenMenu.ino
@@ -1,133 +1,265 @@
+
+
+
+
+
+
+
+
+
 #include <TouchScreenMenu.h>
 #include <TouchScreen.h>
 #include <TFT.h>
 #include <cstddef.h>
 
-TouchScreenForm form1 = TouchScreenForm("Form 1", 2);
+double gasLevel = 0;
+double oilPressure1 = 0;
+double oilPressure2 = 0;
+double tempurature = 0;
+double amps = 0;
+double speed = 0;
+double tach = 0;
+double o2Level = 0;	// 0.9vdc when rich		0.45vdc when good mix				0.1vdc when lean	produces voltage when around 600F
+double mafs = 0;	// 0.4-0.8vdc at idle	4.5-5.0vdc at wide open throttle	Need to make it so the wire is cleaned after the car turns off
+double brakeFluidLevel = 0;
 
-TouchScreenForm form2 = TouchScreenForm("Form 2", 2);
+double cabTemp = 0;
+double outsideTemp = 0;
 
-TouchScreenForm form3 = TouchScreenForm("Timer", 2);
+bool highBeam = false;
+bool charge = false;
+bool parkingBreak = false;
 
-TouchScreenLabel labels[] = {
-  TouchScreenLabel("Big label.", TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 0, 60, 2, 2, true),
-  TouchScreenLabel("ENDOFFORM")
+double intermitent = 0;	// Need to find a good default
+double lowFanSpeed = 0;	// Need to figure out how the fan speeds work and pick a speed for low
+
+// create the array of items for the main menu
+TouchScreenMenuItem mainMenuItems[] = {
+	TouchScreenMenuItem("-> Sub Menus"),
+	TouchScreenMenuItem("Wiper Menu"),
+	TouchScreenMenuItem("ENDOFMENU")
 };
 
-TouchScreenButton buttons[] = {
-  TouchScreenButton("Timer", TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 50, TSC.getScreenHeight() - 100, 2, 10),
-  TouchScreenButton("Next Form", TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 50, TSC.getScreenHeight() - 50, 2, 10),
-  TouchScreenButton("ENDOFFORM")
+// create the array of items for the first sub menu
+TouchScreenMenuItem subMenuItems[] = {
+	TouchScreenMenuItem("<- Main Menu"),
+	TouchScreenMenuItem("Light Control"),
+	TouchScreenMenuItem("Timer"),
+	TouchScreenMenuItem("Test Screen"),
+	TouchScreenMenuItem("Test Screen 2"),
+	TouchScreenMenuItem("Empty Form"),
+	TouchScreenMenuItem("Title-les Form"),
+	TouchScreenMenuItem("ENDOFMENU")
 };
 
-TouchScreenCircleButton circleButtons[] ={
-  TouchScreenCircleButton("Big",TSC.createColor(0, 0, 0), TSC.createColor(255, 255, 255), 10, 30, 70, 70),
-  TouchScreenCircleButton("Small",TSC.createColor(0, 0, 0), TSC.createColor(0, 0, 255), 80, 30, 70, 40),
-  TouchScreenCircleButton("ENDOFFORM")
+// Create the stuff for the timer screen
+TouchScreenMenuItem timerItems[] = {
+	TouchScreenMenuItem("<- Main Menu"),
+	TouchScreenMenuItem("ENDOFMENU")
 };
 
-TouchScreenArrowButton arrowButtons[] = {
-  TouchScreenArrowButton("Up", TSC.createColor(0, 0, 0), TSC.createColor(255, 200, 0), 10, TSC.getScreenHeight() - 180 , 30, 30, UP),
-  TouchScreenArrowButton("Down", TSC.createColor(0, 0, 0), TSC.createColor(255, 0, 255), 50, TSC.getScreenHeight() - 200, 30, 50, DOWN),
-  TouchScreenArrowButton("Left", TSC.createColor(0, 0, 0), TSC.createColor(0, 255, 255), 90, TSC.getScreenHeight() - 220, 30, 70, LEFT),
-  TouchScreenArrowButton("Right", TSC.createColor(0, 0, 0), TSC.createColor(255, 255, 255), 130, TSC.getScreenHeight() - 240, 30, 90, RIGHT),
-  TouchScreenArrowButton("ENDOFFORM")
+// Create an empty menu to be filled with dash data
+TouchScreenMenuItem mainDashItems[] = {
+	TouchScreenMenuItem("ENDOFMENU")
 };
 
-TouchScreenSlider sliders[] = {
-  TouchScreenSlider("Horiz", TSC.createColor(200, 200, 200), TSC.createColor(50, 50, 255), 5, TSC.getScreenHeight() - 110, 150, 40, HORIZONTAL),
-  TouchScreenSlider("Vert", TSC.createColor(255, 0, 0), TSC.createColor(200, 255, 0), TSC.getScreenWidth()-55, TSC.getScreenHeight() - 200, 40, 150, VERTICAL),
-  TouchScreenSlider("ENDOFFORM")
+// // Create menu-less screen
+// TouchScreenMenuItem dimmerItems[] = {
+	// TouchScreenMenuItem("ENDOFMENU")
 };
 
-TouchScreenButton buttons2[] = {
-  TouchScreenButton("<- Back", TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 50, TSC.getScreenHeight() - 50, 2, 10),
-  TouchScreenButton("ENDOFFORM")
+// Create menu-less screen
+TouchScreenMenuItem wiperItems[] = {
+	TouchScreenMenuItem("ENDOFMENU")
 };
 
-// keep track of which form is the currently active one
-TouchScreenForm *curForm = &form1;
+// Create menu-less screen
+TouchScreenMenuItem emptyItems[] = {
+	TouchScreenMenuItem("ENDOFMENU")
+};
 
-//int timer = 0;
-//char timerChar = timer + "";
+unsigned int black = TSC.createColor(0, 0, 0);
+unsigned int white = TSC.createColor(255, 255, 255);
+unsigned int orange = TSC.createColor(206, 114, 33);
 
+// create the various menus setting the (items, font size, spacing, padding, justification, titles)
+TouchScreenMenu subMenu = TouchScreenMenu(subMenuItems, 2, 10, 5, CENTERJ, "Sub Menu");
+TouchScreenMenu mainMenu = TouchScreenMenu(mainMenuItems, 2, 10, 10, CENTERJ, "Main Menu");
+TouchScreenMenu timerScreen = TouchScreenMenu(timerItems, 2, 10, 10, CENTERJ, "Timer Screen");
+TouchScreenMenu dimmerScreen = TouchScreenMenu(emptyItems, 2, 10, 10, CENTERJ, "Dimmer Screen");
+TouchScreenMenu wipperScreen = TouchScreenMenu(wiperItems, 2, 10, 10, CENTERJ, "Wiper Menu");
+TouchScreenMenu TestScreen = TouchScreenMenu(emptyItems, 2, 10, 10, CENTERJ);
+
+// keep track of which menu is the currently active one
+TouchScreenMenu *curMenu = &mainMenu;
+
+TouchScreenArea goBack = TouchScreenButton("<- Back", white, black, 50, TSC.getScreenHeight() - 50, 2, 10);
+
+TouchScreenArrowButton courseDown = TouchScreenArrowButton("courseDown", black, orange, 10, TSC.getScreenHeight() - 180 , 30, 30, LEFT);
+TouchScreenArrowButton courseUp = TouchScreenArrowButton("courseUp", black, orange, 10, TSC.getScreenHeight() - 180 , 30, 30, RIGHT);
+TouchScreenArrowButton fineDown = TouchScreenArrowButton("fineDown", black, orange, 10, TSC.getScreenHeight() - 180 , 30, 30, LEFT);
+TouchScreenArrowButton fineUp = TouchScreenArrowButton("fineUp", black, orange, 10, TSC.getScreenHeight() - 180 , 30, 30, RIGHT);
+
+// TouchScreenSlider(text, foreColor, backColor, x, y, w, h, Layout layout);
+TouchScreenSlider lightBright = TouchScreenSlider("lightBright",TSC.createColor(200, 200, 200), TSC.createColor(50, 50, 255), 5, TSC.getScreenHeight() - 110, 150, 40, HORIZONTAL);
+
+float counter = 0;
+// Resolution is 240 x 320
 void setup(void) {
-  Serial.begin(57000);
-  sliders[0].setValue(.5);
-  sliders[1].setValue(.75);
-  form1.setLabels(labels);
-  form1.setButtons(buttons);
-  form2.setArrowButtons(arrowButtons);
-  form2.setCircleButtons(circleButtons);
-  form2.setSliders(sliders);
-  form2.setButtons(buttons2);
-  form3.setButtons(buttons2);
-
-  TSC.setBackColor(TSC.createColor(255, 255, 0)); // change the default background color
-  TSC.init(); // make sure everything get initialized
-  curForm->draw(); // put up the current form
+	Serial.begin(9600);
+	Serial.println("Starting...");
+	TSC.setBackColor(TSC.createColor(255, 255, 0)); // change the default background color
+	TSC.init(); // make sure everything gets initialized
+	
+	
+	
+	curMenu->draw(); // put up the main menu
 }
 
 void loop(void) {
-  // handle the current form
-  if(curForm!=NULL){
-    // process the current menu
-    TouchScreenArea *item = curForm->process(false);
-    if(item!=NULL){
-      if(curForm == &form1){
-        // move to next form?
-        if(!strcmp(item->getText(),"Next Form")){
-          curForm = &form2;
-          TSC.clearScreen();
-          curForm->draw();
-        }
-        if(!strcmp(item->getText(),"Timer")){
-          curForm = &form3;
-          TSC.clearScreen();
-          curForm->draw();
-        }
-      }else if(curForm == &form2){
-        // move back to the first form?
-        if(!strcmp(item->getText(),"<- Back")){
-          curForm = &form1;
-          TSC.clearScreen();
-          curForm->draw();
-        }else if(!strcmp(item->getText(),"Horiz")){
-          Serial.println(((TouchScreenSlider *)item)->getValue(),DEC);
-          
-        }else if(!strcmp(item->getText(),"Vert")){
-          Serial.println(((TouchScreenSlider *)item)->getValue(),DEC);
-          
-        }else if(!strcmp(item->getText(),"Up")){
-          Serial.println("Up");
-          
-        }else if(!strcmp(item->getText(),"Down")){
-          Serial.println("Down");
-          
-        }else if(!strcmp(item->getText(),"Left")){
-          Serial.println("Left");
-          
-        }else if(!strcmp(item->getText(),"Right")){
-          Serial.println("Right");
-          
-        }else if(!strcmp(item->getText(),"Big")){
-          Serial.println("Big");
-          
-        }else if(!strcmp(item->getText(),"Small")){
-          Serial.println("Small");
-          
-        }
-      }else if(curForm == &form3){
-        if(!strcmp(item->getText(),"<- Back")){
-          curForm = &form1;
-          TSC.clearScreen();
-          curForm->draw();
-        }
-//        form1.setLabels(TouchScreenLabel(timerChar, TSC.createColor(255, 255, 255), TSC.createColor(0, 0, 0), 0, 60, 2, 2, true));
-//        timer = timer + 1;
-//        timerChar = timer+"";
-      }
-    }
-  }
+	// handle the current menu
+	if(curMenu == &timerScreen){
+		// TouchScreenLabel(char *text, unsigned int foreColor, unsigned int backColor, unsigned int x, unsigned int y, unsigned int fontSize, unsigned int padding, bool drawBackground);
+		// void drawString(char *string,unsigned int poX, unsigned int poY,unsigned int size,unsigned int color);
+		char buffer[7];
+		dtostrf(counter, 7, 0, buffer);
+		Serial.println(buffer);
+		TSC.drawString(buffer, 0, 300, 3, TSC.createColor(255, 255, 0));
+		counter = counter + 1;
+		TSC.drawString(buffer, 0, 300, 3, TSC.createColor(0, 0, 0));
+	}else if(curMenu != NULL){
+		// process the current menu
+		TouchScreenMenuItem *item = curMenu->process(false);
+		// check to see which, if any, menu item was pressed
+		checkMenuSelection(item);
+	}else{
+		// if there isn't a current menu being displayed check all of the buttons
+		// to see if any of them was pressed
+		checkButtons();
+	}
+	checkButtons();
+}
+
+// check various buttons and perform actions if any was pressed
+void checkButtons(){
+	if(goBack.process()){ // return from the graphics function screen
+		Serial.println("goBack");
+        curMenu = &mainMenu;
+        TSC.clearScreen();
+        curMenu->draw();
+	}
+	else if(lightBright.process()){ // return from the graphics function screen
+		// Serial.println("lightBright");
+		Serial.print("Brightnes slider Value");
+		float brightness = lightBright.getValue();
+		Serial.print(brightness);
+		Serial.println("");
+		Serial.println(brightness*255);
+		analogWrite(A8, brightness*255);
+		
+	}
+	// else if(backFromBtns.process()){ // return from the buttons screen
+		// Serial.println("backFromBtns");
+        // curMenu = &mainMenu;
+        // TSC.clearScreen();
+        // curMenu->draw();
+  // }
+  // else if(backFromLbls.process()){ // return from the labels screen
+		// Serial.println("backFromLbls");
+        // curMenu = &mainMenu;
+        // TSC.clearScreen();
+        // curMenu->draw();
+  // }
+}
+
+// check to see if any menu item was pressed and do something
+void checkMenuSelection(TouchScreenMenuItem *item) {
+	boolean handled = false;
+	if(item != NULL){
+		Serial.print("Item Clicked: ");
+		Serial.println(item->getText());
+		
+		if(!strcmp(item->getText(),"<- Main Menu")){
+			curMenu = &mainMenu;
+			TSC.clearScreen();
+			curMenu->draw();
+			handled = true;
+		}
+		// main menu items 
+		if(curMenu == &mainMenu){
+			if(!strcmp(item->getText(),"Wiper Menu")){
+				curMenu = &wipperScreen;
+				TSC.clearScreen();
+				curMenu->draw();
+				handled = true;
+			}
+			else if(!strcmp(item->getText(),"-> Sub Menus")){
+				curMenu = &subMenu;
+				TSC.clearScreen();
+				curMenu->draw();
+				handled = true;
+			}
+		}
+		// sub menu items
+		else if(curMenu == &subMenu){
+			if(!strcmp(item->getText(),"Test Screen")){
+				curMenu = &TestScreen;
+				TSC.clearScreen();
+				// 	drawRectangle(poX, poY, width, height, color, boolean fill);
+				for( int y = 0; y < 320; y = y + 20 ) {
+					for( int x = 0; x < 240; x = x + 20 ) {
+						TSC.drawRectangle(x, y, 10, 10, white, true);
+						TSC.drawRectangle(x, y + 10, 10, 10, black, true);
+						TSC.drawRectangle(x + 10, y + 10, 10, 10, white, true);
+						TSC.drawRectangle(x + 10, y, 10, 10, black, true);
+					}
+				}
+				handled = true;
+			}
+			else if(!strcmp(item->getText(),"Test Screen 2")){
+				unsigned long processTime;
+				TSC.clearScreen();
+				// 	drawRectangle(poX, poY, width, height, color, boolean fill);
+				processTime = millis();
+				TSC.drawRectangle(0, 0, 240, 320, white, true);
+				Serial.print("It took ");
+				Serial.print(millis() - processTime);
+				Serial.print(" milliseconds to fill with white.");
+				processTime = millis();
+				Serial.println();
+				
+				TSC.drawRectangle(0, 0, 240, 320, black, true);
+				Serial.print("It took ");
+				Serial.print(millis() - processTime);
+				Serial.print(" milliseconds to fill with black.\n");
+				
+				TSC.clearScreen();
+				curMenu->draw();
+				handled = true;
+			}
+			else if(!strcmp(item->getText(),"Timer")){
+				curMenu = &timerScreen;
+				TSC.clearScreen();
+				curMenu->draw();
+				handled = true;
+			}
+			else if(!strcmp(item->getText(),"Light Control")){
+				lightBright.setValue(.5); // change the value on one of the sliders
+				lightBright.setPadding(10); // change the padding on one of the sliders
+				// btn8.setValue(.75); // change the value on one of the sliders
+				// btn8.setPadding(5); // change the padding on one of the sliders
+				curMenu = &dimmerScreen;
+				TSC.clearScreen();
+				curMenu->draw();
+				goBack.draw();
+				lightBright.draw();
+				handled = true;
+			}
+		}
+		// if the menu item didn't get handled redraw it unpressed
+		if(handled==false){
+			curMenu->drawItem(item,false);
+		}
+	}
 }
 
